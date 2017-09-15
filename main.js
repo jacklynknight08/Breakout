80f01967ecc936af7b70565d2ff849e54e629ed8
jacklynknight08 <jnk08@outlook.com>
2017-09-14 21:05:18 -0500

    initial

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Testing");
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
